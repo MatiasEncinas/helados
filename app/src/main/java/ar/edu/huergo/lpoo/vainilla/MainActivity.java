226c39eaa76af92584d5e2a76837cb1216f9d57c
Francisco Demartino <demartino.francisco@gmail.com>
2020-10-30 18:35:50 -0300

    habilitar cookies en requests de volley

--- app/src/main/java/ar/edu/huergo/lpoo/vainilla/MainActivity.java
+++ app/src/main/java/ar/edu/huergo/lpoo/vainilla/MainActivity.java
@@ -16,6 +16,9 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Activar el uso de cookies en esta app
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://raw.githubusercontent.com/huergo-2020-3ao/helados/master/data-json/helados.json";
@@ -52,5 +57,78 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(req);
+
+
+
+        /*
+        CODIGO DE EJEMPLO PARA VER SI FUNCIONAN LAS COOKIES
+        (pueden ignorarlo, es confuso el orden de los logs)
+
+        queue.add(new JsonObjectRequest(
+                Request.Method.GET, "https://httpbin.org/cookies/set?freeform=hola", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        Log.d("HOLA", "1: tengo estos cookies " + res + " pero acabo de setear hola");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("HOLA", "That didn't work!");
+                Log.d("HOLA", error.getMessage());
+            }
+        }));
+
+        queue.add(new JsonObjectRequest(
+                Request.Method.GET, "https://httpbin.org/cookies", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        Log.d("HOLA", "2: ahora tengo estos cookies " + res);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("HOLA", "That didn't work!");
+                Log.d("HOLA", error.getMessage());
+            }
+        }));
+
+
+        queue.add(new JsonObjectRequest(
+                Request.Method.GET, "https://httpbin.org/cookies/set?freeform=chau", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        Log.d("HOLA", "3: tengo estos cookies " + res + " pero acabo de setear chau");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("HOLA", "That didn't work!");
+                Log.d("HOLA", error.getMessage());
+            }
+        }));
+
+        queue.add(new JsonObjectRequest(
+                Request.Method.GET, "https://httpbin.org/cookies", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        Log.d("HOLA", "4: ahora tengo estos cookies " + res);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("HOLA", "That didn't work!");
+                Log.d("HOLA", error.getMessage());
+            }
+        }));
+        */
+
+
+
+
+
     }
 }
